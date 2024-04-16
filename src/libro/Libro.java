6de02d34abe74fcae3bd51aa5fcb422606ab3dcc
GrupoPoo-2 <167116265+GrupoPoo-2@users.noreply.github.com>
2024-04-15 19:00:11 -0500

    Add files via upload

---
+++ src/libro/Libro.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package libro;
+
+/**
+ *
+ * @author migueldedios
+ */
+// Ejemplo 2: Clase Libro
+public class Libro {
+    private String titulo;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
